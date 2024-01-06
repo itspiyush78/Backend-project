c58ca9008f25ec15d0dad7c8927a496c3b9ce8f7
itspiyush78 <itspiyush78@gmail.com>
2024-01-06 14:35:35 +0530

    update user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,25 +3,15 @@ import {ApiError} from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+  
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
     // check for images, check for avatar
     // upload them to cloudinary, avatar
-<<<<<<< HEAD
-    // create user object
-    
-   
-=======
-    // create user object - create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
->>>>>>> 521a329 (update user controler)
-    
 
-
+ 
     const {fullName, email, username, password } = req.body
     //console.log("email: ", email);
 
