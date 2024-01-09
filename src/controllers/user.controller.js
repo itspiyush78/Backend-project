5bb1f9d82d915e4bc7d8b67f23d079c7cf1f6e5a
itspiyush78 <itspiyush78@gmail.com>
2024-01-09 21:56:19 +0530

    user controller under process

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,10 +7,8 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty
-    // check if user already exists: username, email
-    // check for images, check for avatar
-    
 
+            
  
     const {fullName, email, username, password } = req.body
     //console.log("email: ", email);
