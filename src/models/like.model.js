9cdea758d869132ad57ed028b13d9f3e5d20e681
itspiyush78 <itspiyush78@gmail.com>
2024-01-23 00:17:15 +0530

    Add comment, like, playlist, tweet in models

---
+++ src/models/like.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+const likeSchema = new Schema({
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "video"
+    },
+    Comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    tweet: {
+        type: Schema.Types.ObjectId,
+        ref: "Tweet"
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+}, {timestamps: true});
+
+export const Like = mongoose.model('Like', likeSchema);
